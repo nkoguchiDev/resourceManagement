d00abddfe351c5954793ea1be36f42142e48b74b
nkoguchi <nkoguchi00dev@gmail.com>
2022-02-10 00:03:02 +0900

    WIP

--- .github/workflows/script/wait_neo4j_container.py
+++ .github/workflows/script/wait_neo4j_container.py
@@ -1,8 +1,19 @@
 import requests
+import time
 
 
 def main():
-    pass
+    plotcol = "http"
+    host_name = "localhost"
+    port = 7474
+
+    while True:
+        response = requests.get(f"{plotcol}://{host_name}:{port}")
+
+        if response.status_code == 200:
+            break
+        else:
+            time.sleep(1)
 
 
 if __name__ == "__main__":
