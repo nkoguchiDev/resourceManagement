4227d371ecc4c44046d56cbba05343d48c97514a
nkoguchi <nkoguchi00dev@gmail.com>
2022-02-10 01:53:34 +0900

    feat: add JWT取得module 追加

--- backend/app/app/core/security.py
+++ backend/app/app/core/security.py
@@ -1,15 +1,26 @@
+import secrets
+
 from datetime import datetime, timedelta
 from typing import Any, Union
 
 from jose import jwt
 from passlib.context import CryptContext
 
-from app.core.config import settings
+# from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 ALGORITHM = "HS256"
+
+
+class Settings:
+    # ACCESS_TOKEN_EXPIRE_MINUTES = secrets.token_urlsafe(32)
+    SECRET_KEY = "cce45e4c8450c2781ff1f2e1436cd61fb49c730f5b74b7b4824ca09d77eb89c3"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8
+
+
+settings = Settings()
 
 
 def create_access_token(
