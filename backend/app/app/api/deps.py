c57caf93fd8aafc15a926dc438623a80ab447f9a
nkoguchi <nkoguchi00dev@gmail.com>
2022-02-22 01:21:58 +0900

    feat: SECRET_KEYをconfigから読み込み

--- backend/app/app/api/deps.py
+++ backend/app/app/api/deps.py
@@ -8,17 +8,13 @@ from neo4j import GraphDatabase
 
 from app import crud, models, schemas
 from app.core import security
-# from app.core.config import settings
+from app.core.config import settings
 from app.db.session import SessionLocal
 
 reusable_oauth2 = OAuth2PasswordBearer(
     # tokenUrl=f"{settings.API_V1_STR}/login/access-token"
     tokenUrl="/v1/api/login/access-token"
 )
-
-
-class settings:
-    SECRET_KEY = "cce45e4c8450c2781ff1f2e1436cd61fb49c730f5b74b7b4824ca09d77eb89c3"
 
 
 def get_db() -> Generator:
@@ -42,13 +38,11 @@ def get_current_user(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
         )
-    user = crud.user.get_by_uuid(db, label="test", id=token_data.sub)
+    user = crud.user.get_by_uuid(db, id=token_data.sub)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    elif len(user) > 1:
-        raise HTTPException(status_code=400, detail="Multiple users found")
     else:
-        return user[0]
+        return user
 
 
 def get_current_active_user(
