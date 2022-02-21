197e671f3c4888e1c0209c644a05b4e4f694ed6d
nkoguchi <nkoguchi00dev@gmail.com>
2022-02-22 01:23:59 +0900

    feat: configから読み込む

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -1,7 +1,8 @@
 from neo4j import GraphDatabase
+from app.core.config import settings
 
-
-driver = GraphDatabase.driver("neo4j://localhost:7687",
-                              auth=("neo4j",
-                                    "neo4jj"))
+driver = GraphDatabase.driver(
+    f"neo4j://{settings.GRAPH_DB_HOST}:{settings.GRAPH_DB_PORT}",
+    auth=(settings.GRAPH_DB_USER,
+          settings.GRAPH_DB_PASSWORD))
 SessionLocal = driver.session()
