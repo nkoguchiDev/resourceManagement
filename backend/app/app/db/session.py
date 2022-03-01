c842ebcdd3ff95e299de1f3e98951104f92e4d3a
nkoguchi <nkoguchi00dev@gmail.com>
2022-03-02 01:33:32 +0900

    WIP

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -1,8 +1,13 @@
-from neo4j import GraphDatabase
+# from neo4j import GraphDatabase
 from app.core.config import settings
 
-driver = GraphDatabase.driver(
-    f"neo4j://{settings.GRAPH_DB_HOST}:{settings.GRAPH_DB_PORT}",
-    auth=(settings.GRAPH_DB_USER,
-          settings.GRAPH_DB_PASSWORD))
-SessionLocal = driver.session()
+# driver = GraphDatabase.driver(
+#     f"neo4j://{settings.GRAPH_DB_HOST}:{settings.GRAPH_DB_PORT}",
+#     auth=(settings.GRAPH_DB_USER,
+#           settings.GRAPH_DB_PASSWORD))
+# SessionLocal = driver.session()
+
+from neomodel import db
+auth = f"{settings.GRAPH_DB_USER}:{settings.GRAPH_DB_PASSWORD}"
+SessionLocal = db.set_connection(
+    f'bolt://{auth}@{settings.GRAPH_DB_HOST}:{settings.GRAPH_DB_PORT}')
